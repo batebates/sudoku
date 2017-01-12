21a43e32659308de43678ceee1b584b1294332a7
batebates <baptiste.decrand@laposte.net>
2017-01-12 08:42:35 +0100

    initial project version

---
+++ Test.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#<b>Auteur  :</b> Decrand Baptiste
+#
+#<b>Version :</b> 1.0
+#
+#<b>Date    :</b> 10/01/2017
+#
+#===g
+#<b>Liste des méthodes
+#*
+#</b>
+class Aller
+  #== Variable de classe ==
+  #===========================
+  #== Variables d'instance ==
+  
+  #===========================
+
+  private_class_method :new
+
+  def initialize(oui)#:nodoc:
+  end
+  def Aller.creer(oui)
+    new(oui)
+  end
+end
