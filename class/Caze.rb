3027d55907eb9f098e72518bf4da5d5e135c244f
batebates <baptiste.decrand@laposte.net>
2017-02-24 09:59:32 +0100

    Update Caze.rb

--- class/Caze.rb
+++ class/Caze.rb
@@ -1,10 +1,22 @@
-
-
+#<b>Auteur  :</b> Decrand Baptiste,Medhi Zerbane
+#
+#<b>Version :</b> 1.0
+#
+#<b>Date    :</b> 08/02/2017
+#
+#===Définit une case d'un sudoku
+#<b>Liste des méthodes
+#* to_s
+#* Caze.create
+#</b>
 class Caze
+#== Variables d'instance ==
     @x
     @y
     @value
 	attr_accessor :value
+#==========================
+	
 	def initialize(x,y,value) 
 		@x = x.to_i
         @y = y.to_i
@@ -16,7 +28,8 @@ class Caze
 	def Caze.create(x,y,value)
 		new(x,y,value)
 	end
-	
+
+#===Affiche la valeur de la case	
     def to_s()
         return @value.to_s
     end
