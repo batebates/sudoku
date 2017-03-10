e4ca4929336d74e0d76f39f50cdc776b0a65612c
Apeul <martinsla@live.fr>
2017-03-10 16:58:15 +0100

    Premiere methode pseudocode

--- class/MethodUniqueCandidate.rb
+++ class/MethodUniqueCandidate.rb
@@ -1,3 +1,26 @@
 class MethodUniqueCandidate < Method
 
+	def textMethod
+		return "Dans le cas où un candidat est unique dans une unité, on peut en déduire que la case où il est présent contient bien ce candidat car il ne peut être nul part ailleurs."
+	end
+
+	def demoMethod
+		#TODO
+		#save grille actuelle
+		#creation grille demo
+		#load grille demo
+		#grisation des cases non importantes
+		#Assistant dit on va s'occuper de cette ligne
+		#Mise en valeur de la case
+		#"On peut remarquer que sur cette case machin truc"
+		#affichage du chiffre
+		#"Et voilà le résultat"
+		#Rechargement de la grille normale
+	end
+
+	def onSudokuMethod
+		#TODO
+		#Detection d'un endroit où appliquer la méthode
+		#Coloration de la case + explication de l'assistant
+	end
 end
