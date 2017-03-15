9da5db2aff44c0e27421e93334d8d31a0c123c73
LeSingeAffame <dimitri.bernot@yahoo.fr>
2017-03-15 14:33:46 +0100

    Color load fixed

--- src/save/save.rb
+++ src/save/save.rb
@@ -101,8 +101,9 @@ class Grid < Array
 end
 
 class Sudoku
-	attr_reader :grille_courante
-	attr_reader :grille_resolue
+	attr_reader :gridToComplete
+	attr_reader :gridCompleted
+	attr_reader :colorList
 	
 	private_class_method :new
 	
@@ -112,30 +113,86 @@ class Sudoku
 	end
 	
 	def initialize(fileName)
-		@grille_courante = Grid.new()
-		@grille_resolue = Grid.new()
+		@gridToComplete = Grid.new()
+		@gridCompleted = Grid.new()
+		@colorList = Array.new(4)
+		@grilleCouleurs = Grid.new()
 		
 		loadSudoku(fileName)
 	end
 	
 	# Puts the content of a string into the grid to be solved
 	def gridModify(sudokuString)
-		@grille_courante.stringToGrid(sudokuString)
+		@gridToComplete.stringToGrid(sudokuString)
 	end
 	
 	# Puts the content of a string into the solved grid
 	def completedModify(sudokuString)
-		@grille_resolue.stringToGrid(sudokuString)
+		@gridCompleted.stringToGrid(sudokuString)
 	end
 	
-	def afficher()
-		@grille_courante.afficher()
-		@grille_resolue.afficher()
+	# Returns the color list, in RGB
+	def getColorList()
+		return @colorList
+	end
+	
+	# Returns a color in particular, in RGB
+	def getColor(colorNb)
+		return @colorList[colorNb]
+	end
+	
+	# Returns the amount of red in a color
+	def getRed(colorNb)
+		return getColor(colorNb)[0]
+	end
+	
+	# Returns the amount of red in a color
+	def getGreen(colorNb)
+		return getColor(colorNb)[1]
+	end
+	
+	# Returns the amount of red in a color
+	def getBlue(colorNb)
+		return getColor(colorNb)[2]
+	end
+	
+	def setColor(*args)
+		if args.length == 3 then # i, couleur, quantite
+			if args[1].to_i == 0 then
+				setRed(args[0], args[2].to_i)
+			elsif args[1].to_i == 1 then
+				setGreen(args[0], args[2])
+			elsif args[1].to_i == 2 then
+				setBlue(args[0], args[2])
+			end
+		elsif args.length == 4 then # i, rouge, vert, bleu
+			setRed(args[0],args[1])
+			setGreen(args[0],args[2])
+			setBlue(args[0],args[3])
+		end
+	end
+	
+	def setRed(i,r)
+		@colorList[i][0] = r
+		
+		return self
+	end
+	
+	def setGreen(i,g)
+		@colorList[i][1] = g.to_i
+		
+		return self
+	end
+	
+	def setBlue(i,b)
+		@colorList[i][2] = b.to_i
+		
+		return self
 	end
 	
 	# Checks if the sent value would be the correct one in the specified cell
 	def isCorrectValue?(x, y, value)
-		return @grille_resolue.getValue(x, y) == value
+		return @gridCompleted.getValue(x, y) == value
 	end
 	
 	# Checks if the specified cell is either correct or left blank
@@ -148,7 +205,7 @@ class Sudoku
 			cellY = args[1]
 		end
 		
-		return @grille_resolue.getValue(cellX, cellY) == @grille_courante.getValue(cellX, cellY) || @grille_courante.getValue(cellX, cellY) == 0
+		return @gridCompleted.getValue(cellX, cellY) == @gridToComplete.getValue(cellX, cellY) || @gridToComplete.getValue(cellX, cellY) == 0
 	end
 	
 	# Checks if each cell of the first grid is either correct or left blank
@@ -173,14 +230,16 @@ class Sudoku
 		end
 		
 		for i in 0..80
-			saveFile.write self.grille_courante[i].getValue()
+			saveFile.write self.gridToComplete[i].getValue()
 		end
 		
-		saveFile.write "\n\n"
+		saveFile.write "\n"
 		
 		for i in 0..80
-			saveFile.write self.grille_resolue[i].getValue()
+			saveFile.write self.gridCompleted[i].getValue()
 		end
+		
+		saveFile.write "\n"
 		
 		saveFile.close
 		
@@ -197,9 +256,26 @@ class Sudoku
 			print "Fichier à charger ouvert\n"
 		end
 		
-		sudokuToBeCompleted = IO.read(loadFile, 81)
-		sudokuCompleted = IO.read(loadFile, 81, 85)
-		#contents = loadFile.read(
+		fileContent = IO.readlines(fileName)
+		
+		# Grids
+		sudokuToBeCompleted = fileContent[0]
+		sudokuCompleted = fileContent[1]
+		
+		colorListBuffer = Array.new(4,Array.new(3,0))
+		
+		# Color code
+		for i in 0..3
+			colorList = fileContent[3+i]
+			colors = colorList.gsub(/,+/m, ',').strip.split(",")
+			
+			@colorList[i] = Array.new(3)
+			
+			for j in 0..2
+				@colorList[i][j] = (colors[j].to_i / 255.0).round(2)
+			end
+		end
+		
 		self.gridModify(sudokuToBeCompleted)
 		self.completedModify(sudokuCompleted)
 		
@@ -210,27 +286,39 @@ class Sudoku
 		end
 	end
 	
+	### PRINT
+	def colorPrint()
+		for i in 0..3
+			print "Couleur " + i.to_s + " : " + getColor(i).to_s + "\n"
+		end
+	end
+	
+	def afficher()
+		#@gridToComplete.afficher()
+		print "\n"
+		#@gridCompleted.afficher()
+		print "\n"
+		colorPrint()
+	end
+	
 	### TESTS
 	def Sudoku.test()
 		sudokuFile = "./save_files/sudoku.txt"
 		
+=begin
 		if system('save_files/sudoku_generator.exe', sudokuFile) == false then
 			print "Error during the sudoku generation process"
 		end
+=end
 
 		###Saves
 		#Read
 		sudokuRead = Sudoku.lire(sudokuFile)
-		print "Grille courante : \n"
-		sudokuRead.grille_courante.afficher()
-
-		print "\nGrille résolue : \n"
-		sudokuRead.grille_resolue.afficher()
-		print "\n"
+		sudokuRead.afficher()
 
 		### Tests
 		print "\nValeur en 3, 5 : 9 ?\n"
-		print sudokuRead.grille_resolue.getValue(3,5), " - ", sudokuRead.isCorrectValue?(3, 5, 9),  "\n"
+		print sudokuRead.gridCompleted.getValue(3,5), " - ", sudokuRead.isCorrectValue?(3, 5, 9),  "\n"
 
 		print "\nFirst grid correct ? (Should be TRUE)\n"
 		print sudokuRead.isCorrectGrid?(), "\n"
