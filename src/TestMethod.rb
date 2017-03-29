b812ede158d792f42b227e3aca4d725e984a45c7
Apeul <martinsla@live.fr>
2017-03-29 15:33:17 +0200

    testmethod

---
+++ src/TestMethod.rb
@@ -0,0 +1,37 @@
+require "gtk3"
+require "gio2"
+require "gdk3"
+require "pango"
+require "observer"
+
+require "./view/Window.rb"
+require "./view/GridView.rb"
+require "./view/SquareView.rb"
+require "./view/AssistantView.rb"
+require "./view/CSSStyle.rb"
+require "./view/Colors.rb"
+require "./view/OverlayManager.rb"
+require "./view/Header.rb"
+require "./view/Menu.rb"
+require "./view/OptionsDialog.rb"
+
+require "./model/AssetManager.rb"
+require "./model/Generator.rb"
+require "./model/Caze.rb"
+require "./model/Sudoku.rb"
+
+require "./controller/SudokuAPI.rb"
+=begin
+
+require "./controller/MethodCrossReduce.rb"
+require "./controller/MethodGroupIsolated.rb"
+require "./controller/MethodInteractionsRegion.rb"
+require "./controller/MethodTwinsAndTriplets.rb"
+
+=end
+
+require "./controller/Methode.rb"
+require "./controller/MethodUniqueCandidate.rb"
+
+
+testd = MethodUniqueCandidate.new;
