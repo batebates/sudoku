3b092cc53a6fb7d81d7a0bb52c84fae804f34def
mehdi zerbane <spi3154@ic2s114-16.univ-lemans.fr>
2017-03-30 10:53:44 +0200

    Modif method

--- src/controller/MethodTwinsAndTriplets.rb
+++ src/controller/MethodTwinsAndTriplets.rb
@@ -1,14 +1,14 @@
 class MethodTwinsAndTriplets
 	
 	def textMethod
-		SudokuAPI.API.assistantMessage("Cette méthode nous permet de déduire dans quelle partie d'une région peut se trouver un symbole. En effet il n'est pas toujours évident de découvrir dès le début l'emplacement final et définitif. Si un candidat se trouve uniquement par exemple dans la dernière ligne d'une région il se trouvera donc obligatoirement dans cette ligne.")
+		SudokuAPI.API.assistantMessage=("Cette méthode nous permet de déduire dans quelle partie d'une région peut se trouver un symbole. En effet il n'est pas toujours évident de découvrir dès le début l'emplacement final et définitif. Si un candidat se trouve uniquement par exemple dans la dernière ligne d'une région il se trouvera donc obligatoirement dans cette ligne.")
 	end
 
 	def demoMethod
 		gridDemo = "375648129010925070200371000732089060000267000060034792020453917147896235953712648"	
 
 		new_=SudokuAPI.API
-		new_.setSudoku(Sudoku.create(gridDemo));
+		new_.setSudoku(Sudoku.create(gridDemo),Sudoku.create(gridDemo),Sudoku.create(gridDemo));
 
 		new_.assistantMessage=("Bienvenue dans la démo");
 		sleep(0.5);
@@ -23,7 +23,7 @@ class MethodTwinsAndTriplets
 		new_.assistantMessage=("Les 2 candidats 4, alignés dans cette région (en rouge), donnent la possibilité de supprimer les 4 dans les autres régions de cette ligne (en gris)");
 		sleep(2);
 
-		Window.init();
+		
 
 	end
 
