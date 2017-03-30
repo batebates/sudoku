752cc2e3f2aa2c0b7312dae5111bc978e282e871
mehdi zerbane <spi3154@ic2s114-16.univ-lemans.fr>
2017-03-30 11:43:28 +0200

    Ajout type

--- src/controller/MethodTwinsAndTriplets.rb
+++ src/controller/MethodTwinsAndTriplets.rb
@@ -1,13 +1,16 @@
 class MethodTwinsAndTriplets
 	
 	@step = 0
+	@type = "textMethod"
 	
 	def textMethod
+		@type = "textMethod"
 		SudokuAPI.API.assistantMessage=("Cette méthode nous permet de déduire dans quelle partie d'une région peut se trouver un symbole. En effet il n'est pas toujours évident de découvrir dès le début l'emplacement final et définitif. Si un candidat se trouve uniquement par exemple dans la dernière ligne d'une région il se trouvera donc obligatoirement dans cette ligne.")
 		@step+=1
 	end
 
 	def demoMethod
+		@type = "demoMethod"
 		SudokuAPI.API.saveSudoku("old");
 		gridDemo = "375648129010925070200371000732089060000267000060034792020453917147896235953712648"	
 
@@ -29,12 +32,13 @@ class MethodTwinsAndTriplets
 		SudokuAPI.API.loadSudoku("old");
 		@step+=1
 		
-
 	end
 
 	def onSudokuMethod(sudokuAPI)
+		@type = "onSudokuMethod"
 		sudokuAPI.assistantMessage=("Nous commençons par rechercher un candidat présent uniquement peut importe le nombre de fois dans une ligne dans une région");
 		@step+=1
+
 	end
 
 	
