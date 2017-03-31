9add52cc6000f6d7f2701928ce72f13ce67350fd
Mehdi <mehdi72320@live.fr>
2017-03-31 14:48:18 +0200

    Step

--- src/controller/MethodTwinsAndTriplets.rb
+++ src/controller/MethodTwinsAndTriplets.rb
@@ -16,40 +16,37 @@ class MethodTwinsAndTriplets
 
 	def demoMethod
 		@type = "demoMethod"
-		SudokuAPI.API.saveSudoku("old");
-		gridDemo = "375648129010925070200371000732089060000267000060034792020453917147896235953712648"	
+		if(@step==0)
+			SudokuAPI.API.saveSudoku("old");
+			gridDemo = "375648129010925070200371000732089060000267000060034792020453917147896235953712648"	
+			SudokuAPI.API.setSudoku(Sudoku.create(gridDemo),Sudoku.create(gridDemo),Sudoku.create(gridDemo));
+			SudokuAPI.API.assistantMessage=("Bienvenue dans la démo (cliquez sur suivant pour continuer");
+		elsif(@step==1)
+			SudokuAPI.API.cazeAt(0,4).color=Colors::CL_NUMBER_LOCKED;
+			SudokuAPI.API.cazeAt(2,4).color=Colors::CL_NUMBER_LOCKED;
+			SudokuAPI.API.cazeAt(6,4).color=Colors::CL_NUMBER;
+			SudokuAPI.API.cazeAt(8,4).color=Colors::CL_NUMBER;
 
+			SudokuAPI.API.enableHint(true);
 
-		SudokuAPI.API.setSudoku(Sudoku.create(gridDemo),Sudoku.create(gridDemo),Sudoku.create(gridDemo));
-
-		SudokuAPI.API.assistantMessage=("Bienvenue dans la démo");
-
-
-		SudokuAPI.API.cazeAt(0,4).color=Colors::CL_NUMBER_LOCKED;
-		SudokuAPI.API.cazeAt(2,4).color=Colors::CL_NUMBER_LOCKED;
-		SudokuAPI.API.cazeAt(6,4).color=Colors::CL_NUMBER;
-		SudokuAPI.API.cazeAt(8,4).color=Colors::CL_NUMBER;
-
-		SudokuAPI.API.enableHint(true);
-
-
-		SudokuAPI.API.assistantMessage=("Les 2 candidats 4, alignés dans cette région (en rouge), donnent la possibilité de supprimer les 4 dans les autres régions de cette ligne (en gris)");
-		
-		SudokuAPI.API.loadSudoku("old");
-
+			SudokuAPI.API.assistantMessage=("Les 2 candidats 4, alignés dans cette région (en rouge), donnent la possibilité de supprimer les 4 dans les autres régions de cette ligne (en gris)");
+		elsif(@step==2)	
+			SudokuAPI.API.loadSudoku("old");
+			SudokuAPI.API.assistantMessage=("Bonjour, je suis l'assistant, je suis là pour vous aider");
+		end
 		@step+=1
 	end
 
 	def onSudokuMethod
 		@type = "onSudokuMethod"
 		SudokuAPI.API.assistantMessage=("Nous commençons par rechercher un candidat présent uniquement peut importe le nombre de fois dans une ligne dans une région");
-
+		SudokuAPI.API.enableHint(false);
 
 		@step+=1
+	end
 
-
-
-
+	def update
+		self.demoMethod()
 	end
 
 	
