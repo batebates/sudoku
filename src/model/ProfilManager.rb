b6eb7f7216acb148264c3c7248d1908820f1e480
godefroy thieulart <spi3152@ic2s124-10.univ-lemans.fr>
2017-04-04 14:43:23 +0200

    version final des profils

--- src/model/ProfilManager.rb
+++ src/model/ProfilManager.rb
@@ -1,3 +1,5 @@
+require "yaml"
+
 # encoding: UTF-8
 
 ##
@@ -15,26 +17,36 @@ class ProfilManager
 	#
 	#=== Paramètres:
 	#<b>pseudo</b>  	: pseudo du joueur
-	def ajouter(pseudo)
+	#
+	#=== Return :
+	#<b>return vrai si l'ajout est fait, faux sinon</b>
+	def ProfilManager.ajouter(pseudo)
 		if(!self.existe(pseudo))
 			@@listeProfil.push(pseudo)
 			self.save()
+			return true
 		else
 			puts "Pseudo deja pris"
 		end
+		return false
 	end
 
 	#=== Methode de classe permettant la suppression d'un profile
 	#
 	#=== Paramètres:
 	#<b>pseudo</b>  	: pseudo du joueur
-	def supprimer(pseudo)
+	#
+	#=== Return :
+	#<b>return vrai si la suppression est faite, faux sinon</b>
+	def ProfilManager.supprimer(pseudo)
 		if(self.existe(pseudo))
 			@@listeProfil.delete_at(@@listeProfil.index(pseudo))
 			self.save()
+			return true
 		else
 			puts "Pseudo non présent"
 		end
+		return false
 	end
 
 	#=== Methode de classe permettant de renommer un profil
@@ -42,23 +54,29 @@ class ProfilManager
 	#=== Paramètres:
 	#<b>oldName</b>  	: pseudo actuel du profil
 	#<b>newName</b>		: nouveau pseudo du profil
-	def rename(oldName, newName)
-		if(self.existe(oldName))
+	#
+	#=== Return :
+	#<b>return vrai si le pseudo a été renommer, faux sinon</b>
+	def ProfilManager.rename(oldName, newName)
+		if(self.existe(oldName) && !self.existe(newName))
 			@@listeProfil[@@listeProfil.index(oldName)] = newName;
 			if(@@dernierPseudo == oldName)
 				@@dernierPseudo = newname
 			end
 			self.save();
+			return true
 		end
+		return false
 	end
 
 	#=== Methode de classe permettant de connecter un joueur
 	#
 	#=== Paramètres:
 	#<b>nom</b>  	: nom du joueur qui se connecte
-	def connecter(nom)
+	def ProfilManager.connecter(nom)
 		if(self.existe(nom))
 			@@dernierPseudo = nom;
+			self.save();
 		else
 			puts "Pseudo non présent"
 		end
@@ -66,8 +84,14 @@ class ProfilManager
 
 	#=== Methode de classe permettant de recuperer le pseudo du dernier joueur
 	#
-	def dernierJoueur()
+	def ProfilManager.dernierJoueur()
 		return @@dernierPseudo
+	end
+
+	#=== Methode de classe permettant de recuperer la liste des pseudo
+	#
+	def ProfilManager.listeProfile()
+		return @@listeProfile
 	end
 
 	#=== Methode permettant de tester l'existence d'un profil
@@ -77,57 +101,48 @@ class ProfilManager
 	#
 	#=== Return :
 	#<b>return vrai si le pseudo est présent, faux sinon</b>
-	def existe(nom)
+	def ProfilManager.existe(nom)
 		@@listeProfil.index(nom) != nil;
 	end
 
 	#=== Methode Sauvegardant les profiles dans un fichier txt
 	#
-	def save()
-		file = open("saveProfil", "w")
-
-		if(!file.closed?)
-			#print "Sauvegarde des profiles...\n"
+	def ProfilManager.save()
+		userFile = File.new("users.yml","w")
+        
+		if(!userFile.closed?)
+            puts "Ouverture liste utilisateur\n"
 		end
+	
+		dumpArray = [];
+		dumpArray.push(@@dernierPseudo);
+		dumpArray.push(@@listeProfil);
 
-		file.write(@@dernierPseudo.to_s() + "\n")
+		userFile.puts YAML::dump(dumpArray)
+		userFile.close
 
-		@@listeProfil.each { |profil|
-			file.write(profil + "\n")
-		}
-
-		file.close()
-		if(file.closed?)
-			#print "Sauvegarde des profiles terminées!\n"
+        if(!userFile.closed?)
+            puts "Fermeture liste utilisateur\n"
 		end
 	end
 
 	#=== Methode de classes permettant de charger un tableau de profil a partir d'un fichier
 	#
-	def loadFile()
-		profilList = File.open("saveProfil", "r")
-		cpt=0
+	def ProfilManager.loadFile()
+		if(!File.file?("users.yml"))
+            return;
+		end
 
-		if(!profilList.closed?)
-			#print "Chargement des profiles...\n"
-		end
-	
-		@@dernierPseudo = profilList.readline()
-		profilList.each_line{ |ligne|
-			if(!ligne.empty?())			
-				@@listeProfil.push(ligne)
-			end
-		}	
-		
-		profilList.close()
-		if(profilList.closed?)
-			#print "Profiles chargés !\n"
-		end
+		userFile = YAML.load_file("users.yml")
+		if(userFile)
+			@@dernierPseudo = userFile[0];
+			@@listeProfil = userFile[1];
+		end	
 	end
 
 	#===Affiche le tableau des profiles
 	#
-	def toAff()
+	def ProfilManager.toAff()
 		print "\n Affichage des profils :\n"
 		@@listeProfil.each { |profil|
 			puts profil
@@ -138,21 +153,19 @@ end
 
 
 profil = ProfilManager.new()
-profil.loadFile()
-profil.ajouter("Valentin")
-profil.ajouter("Dimitri")
-profil.toAff()
+ProfilManager.loadFile()
+ProfilManager.ajouter("Valentin")
+ProfilManager.ajouter("Dimitri")
+ProfilManager.toAff()
 
-#profil.supprimer("Valentin")
-#profil.rename("Dimitri", "DemiTruie")
-#profil.ajouter("Valentin 2 : Le retour")
-#profil.toAff()
+ProfilManager.supprimer("Valentin")
+ProfilManager.rename("Dimitri", "The Mighty Dimitri")
+ProfilManager.ajouter("Valention The Boss")
+ProfilManager.toAff()
 
 
-
-#puts"Dernier Joueur = "+profil.dernierJoueur()
-#profil.connecter("DemiTruie")
-#puts"Dernier Joueur = "+profil.dernierJoueur()
+ProfilManager.connecter("The Mighty Dimitri")
+puts"\nDernier Joueur = "+ProfilManager.dernierJoueur().to_s
 
 
 
